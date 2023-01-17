414f6d851ef20ac2cdb47157001f0d710e0d19bf
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-01-17 08:46:53 -0600

    practice coffee project

---
+++ Javascript/main.js
@@ -0,0 +1,27 @@
+//variables for coffee images
+let imgLight = "img/lightRoast.svg";
+let imgMedium = "img/mediumRoast.svg";
+let imgDark = "img/darkRoast2.svg";
+
+//variables for coffee description
+let light = "Light brown in color, this roast is generally preferred for milder coffee varieties.";
+let medium = "This roast is medium brown in color with a stronger flavor and a non-oily surface.";
+let dark = "This roast produces shiny black beans with an oily surface and a pronounced bitterness.";
+
+//coffees array from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light',description:light, image:imgLight},
+    {id: 2, name: 'Half City', roast: 'light',description:light, image:imgLight},
+    {id: 3, name: 'Cinnamon', roast: 'light',description:light, image:imgLight},
+    {id: 4, name: 'City', roast: 'medium',description:medium, image:imgMedium},
+    {id: 5, name: 'American', roast: 'medium',description:medium, image:imgMedium},
+    {id: 6, name: 'Breakfast', roast: 'medium',description:medium, image:imgMedium},
+    {id: 7, name: 'High', roast: 'dark', description:dark, image:imgDark},
+    {id: 8, name: 'Continental', roast: 'dark',description:dark, image:imgDark},
+    {id: 9, name: 'New Orleans', roast: 'dark',description:dark, image:imgDark},
+    {id: 10, name: 'European', roast: 'dark',description:dark, image:imgDark},
+    {id: 11, name: 'Espresso', roast: 'dark',description:dark, image:imgDark},
+    {id: 12, name: 'Viennese', roast: 'dark',description:dark, image:imgDark},
+    {id: 13, name: 'Italian', roast: 'dark',description:dark, image:imgDark},
+    {id: 14, name: 'French', roast: 'dark',description:dark, image:imgDark},
+];
